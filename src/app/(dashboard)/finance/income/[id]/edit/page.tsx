ef453f49b5d895d5faeeef307506bc20fb35bc2f
Driann's <114148680+Driannm@users.noreply.github.com>
2026-02-04 18:29:43 +0700

    fix: hosting build error

--- src/app/(dashboard)/finance/income/[id]/edit/page.tsx
+++ src/app/(dashboard)/finance/income/[id]/edit/page.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useRouter } from "next/navigation";
 import UnderConstruction from "@/components/UnderConstruction";
 
 export default function IncomeEditPage() {
