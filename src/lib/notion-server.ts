96ea9e2ec2e75e27a255f46033cb22a4e119b95f
Driann's <114148680+Driannm@users.noreply.github.com>
2026-03-04 20:52:22 +0700

    update: add notes_database_id on notion-server.ts

--- src/lib/notion-server.ts
+++ src/lib/notion-server.ts
@@ -4,8 +4,12 @@ export const notion = new Client({
   auth: process.env.NOTION_KEY,
 });
 
+// existing exports (unchanged)
 export const DATABASE_ID = process.env.EXPENSES_DATABASE_ID!;
 export const TEMPLATE_DATABASE_ID = process.env.EXPENSES_TEMPLATE_DATABASE_ID!;
 export const INCOME_DATABASE_ID = process.env.INCOME_DATABASE_ID!;
 export const CARD_DATABASE_ID = process.env.CARD_DATABASE_ID!;
 export const TRANSFERS_DATABASE_ID = process.env.TRANSFERS_DATABASE_ID!;
+
+// notes
+export const NOTES_DATABASE_ID = process.env.NOTES_DATABASE_ID!;
